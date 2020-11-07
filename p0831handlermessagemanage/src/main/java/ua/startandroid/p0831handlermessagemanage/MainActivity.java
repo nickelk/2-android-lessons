8cf216acd9b6f6ecf9a6829204c24aab63fa5937
nickelk <nick_smith@ukr.net>
2020-11-07 10:55:14 +0200

    handler message managed

---
+++ p0831handlermessagemanage/src/main/java/ua/startandroid/p0831handlermessagemanage/MainActivity.java
@@ -0,0 +1,44 @@
+package ua.startandroid.p0831handlermessagemanage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    Handler h;
+
+    Handler.Callback hc = new Handler.Callback() {
+        public boolean handleMessage(Message msg) {
+            Log.d(LOG_TAG, "what = " + msg.what);
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        h = new Handler(hc);
+        sendMessages();
+    }
+
+    void sendMessages() {
+        Log.d(LOG_TAG, "send messages");
+        h.sendEmptyMessageDelayed(1, 1000);
+        h.sendEmptyMessageDelayed(2, 2000);
+        h.sendEmptyMessageDelayed(3, 3000);
+        h.sendEmptyMessageDelayed(2, 4000);
+        h.sendEmptyMessageDelayed(5, 5000);
+        h.sendEmptyMessageDelayed(2, 6000);
+        h.sendEmptyMessageDelayed(7, 7000);
+        h.removeMessages(2);
+    }
+
+}
